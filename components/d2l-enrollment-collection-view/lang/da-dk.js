453619cb3d26d91e62c60688455ddc1ba4465781
JenkinsReader <resupport@desire2learn.com>
2020-11-27 11:39:16 -0500

    Translation Updates

---
+++ components/d2l-enrollment-collection-view/lang/da-dk.js
@@ -0,0 +1,13 @@
+/* eslint quotes: 0 */
+
+export default {
+	"courseEnrollmentType": "Kursus", //  The name of the Course org unit type
+	"learningPathEnrollmentType": "Undervisningssti", // The name of the Learning Path org unit type
+	"loadMore": "Indlæs flere", // The button that will load additional learning items,
+	"myLearning": "Min undervisning", // My learning pages title
+	"noLearning": "Du har ingen kurser eller undervisningsstier, der skal vises.", // The message that is displayed when a user has no learning items to show
+	"noSearchResults": "0 søgeresultater fundet for \"{searchTerm}\"", // The message that is displayed when a search for learning items returns no results
+	"searchCompletedOne": "Der blev fundet {count} søgeresultater.", // The announcement when a search returns one page of results
+	"searchCompletedPaged": "Der blev fundet mere end {count} søgeresultater.", // The announcement when a search returns more than one page of results
+	"searchPlaceholder": "Søg ...", // Placeholder text for the search input to search the list learning items
+};
