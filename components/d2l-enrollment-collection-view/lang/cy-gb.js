963675366dd90078c0ed2661215e15f592b0e941
JenkinsReader <resupport@desire2learn.com>
2020-12-02 12:38:33 -0500

    Translation Updates

---
+++ components/d2l-enrollment-collection-view/lang/cy-gb.js
@@ -0,0 +1,13 @@
+/* eslint quotes: 0 */
+
+export default {
+	"courseEnrollmentType": "Cwrs", //  The name of the Course org unit type
+	"learningPathEnrollmentType": "Llwybr Dysgu", // The name of the Learning Path org unit type
+	"loadMore": "Llwytho Mwy", // The button that will load additional learning items,
+	"myLearning": "Fy Nysgu", // My learning pages title
+	"noLearning": "Does gennych chi ddim cyrsiau na llwybrau dysgu i’w dangos.", // The message that is displayed when a user has no learning items to show
+	"noSearchResults": "Canfuwyd 0 canlyniad chwilio ar gyfer \"{searchTerm}\"", // The message that is displayed when a search for learning items returns no results
+	"searchCompletedOne": "Canfuwyd {count} o ganlyniadau chwilio.", // The announcement when a search returns one page of results
+	"searchCompletedPaged": "Canfuwyd mwy na {count} o ganlyniadau chwilio.", // The announcement when a search returns more than one page of results
+	"searchPlaceholder": "Chwilio...", // Placeholder text for the search input to search the list learning items
+};
